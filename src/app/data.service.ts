7816e8028690c85833faf9aeea953420fa3d455a
Jose Mata <rafaelmm899@hotmail.com>
2019-11-30 00:37:10 -0300

    remove comments

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -30,21 +30,5 @@ export class DataService implements InMemoryDbService {
                     return { products: result[0].products }
                 }
             );
-
-        /*let products = [
-            { quantity: 514, price: "$11,042", availability: false, name: "dolor", id: 1 },
-            { quantity: 887, price: "$17,532", availability: true, name: "dolor", id: 2 },
-            { quantity: 202, price: "$6,174", availability: true, name: "enim", id: 3 },
-            { quantity: 700, price: "$1,904", availability: false, name: "ad", id: 4 },
-            { quantity: 343, price: "$14,388", availability: false, name: "ullamco", id: 5 },
-            { quantity: 435, price: "$6,809", availability: false, name: "mollit", id: 6 }
-        ];
-
-        return { products };*/
-
     }
-
-    /*genId(): number {
-        return Math.round(Math.random() * 1000000);
-    }*/
 }
